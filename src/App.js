34f61ace716205d28dd986577b244111a10ced2c
tareque-bin-habib <shad.tbh@gmail.com>
2023-03-09 12:35:16 +0600

    environment setup complite

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      kavod
-      Syeda
+    <div>
+      <button className="btn">Button</button>
+      <button className="btn btn-primary">Button</button>
+      <button className="btn btn-secondary">Button</button>
+      <button className="btn btn-accent">Button</button>
+      <button className="btn btn-ghost">Button</button>
+      <button className="btn btn-link">Button</button>
     </div>
   );
 }
